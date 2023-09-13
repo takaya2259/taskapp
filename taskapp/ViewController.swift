405c9031d9f3bedf54e053176044e2591c0b339f
takaya <tky.ishimori@gmail.com>
2023-09-13 11:40:02 +0900

    画面全体にTableViewを表示

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.fillerRowHeight = UITableView.automaticDimension
     }
 
 
