fd027a7f0013487ad911443e7807f84ccd271b04
takaya <tky.ishimori@gmail.com>
2023-09-13 16:26:33 +0900

    Inputファイルを作成

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by 石森貴也 on 2023/09/13.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
